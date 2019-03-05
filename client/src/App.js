381446013640f8d74c3404e75f7794df005ffb1a
hua zhou <huazhou2@gmail.com>
2019-03-05 14:12:31 -0500

    remove unnecessary

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="containter">
+        <header>
+          <p>
+	    this is my webpage
+          </p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
